d568d78eda81cafde6a9781474222e51839be4b8
MoDiouf <mohameddiouf096@gmail.com>
2025-02-10 13:21:09 +0000

    Okayy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index') // Assurez-vous que le fichier "acceuil.ejs" existe dans "views"
+  home() {
+    return { message: 'Bienvenue sur JollofNet !' }; // 👈 Passer des données à EJS
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setViewEngine('ejs');
+  app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
+
+  await app.listen(3000);
 }
 bootstrap();
