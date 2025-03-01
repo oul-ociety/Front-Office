1a5227b6d8e7355c18c2381317884701c9f15dc2
MoDiouf <mohameddiouf096@gmail.com>
2025-03-01 01:19:43 +0000

    okayy

--- src/connexion/connexion.module.ts
+++ src/connexion/connexion.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConnexionService } from './connexion.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Electeur } from 'src/entities/Electeur.entity';
+import { DashElecteurModule } from 'src/dash-electeur/dash-electeur.module';
+import { ConnexionController } from './connexion.controller';
 
 @Module({
-  providers: [ConnexionService]
+  imports: [
+    TypeOrmModule.forFeature([Electeur]),  // <-- Nécessaire si non géré dans ElecteurModule
+    DashElecteurModule  // <-- Ajout du module qui contient ElecteurRepository
+  ],
+  providers: [ConnexionService],   // Fournir le service
+  exports: [ConnexionService],     // Exporter pour d'autres modules
+  controllers: [ConnexionController],
 })
 export class ConnexionModule {}
