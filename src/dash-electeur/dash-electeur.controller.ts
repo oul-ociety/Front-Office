45c8f306f79b129ee68c0c1e1b125382c1e80482
MoDiouf <mohameddiouf096@gmail.com>
2025-03-02 17:52:09 +0000

    NdiayeDIouf

--- src/dash-electeur/dash-electeur.controller.ts
+++ src/dash-electeur/dash-electeur.controller.ts
@@ -1,19 +1,50 @@
-import { Controller, Get, Render, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post, Render, Req, Body, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { DashElecteurService } from './dash-electeur.service';
 
 @Controller('dashboard/electeur')
 export class DashElecteurController {
+  constructor(private readonly candidatService: DashElecteurService) {}
 
-    @Get()
-    @Render('dashboardElecteur') 
-    home(@Req() req: Request) {
-        const currentPage = req.url.replace('/dashboard/electeur', '') || '/homecandidat';
+  @Get()
+  @Render('dashboardElecteur')
+  async home(@Req() req: Request) {
+    const currentPage =
+      req.url.replace('/dashboard/electeur', '') || '/homecandidat';
+    const candidats = await this.candidatService.getAllCandidats();
 
-        console.log("Parrainer");
+    return {
+      titre: 'Parrainer',
+      currentUrl: currentPage,
+      candidats,
+    };
+  }
 
-        return { 
-            titre: "Parrainer",
-            currentUrl: currentPage 
-        };
+  @Post('send-code')
+  async sendCode(@Body() body: any, @Res() res: Response) {
+    const { email } = body;
+
+    // Envoyer le code à l'e-mail de l'utilisateur
+    const success = await this.candidatService.generateAndSendCode(email);
+
+    if (success) {
+      res.json({ success: true, message: 'Code envoyé avec succès !' });
+    } else {
+      res.json({ success: false, message: "Échec de l'envoi du code." });
     }
+  }
+
+  @Post('verify-code')
+  async verifyCode(@Body() body: any, @Res() res: Response) {
+    const { email, code } = body;
+
+    // Vérifier le code saisi
+    const isValid = await this.candidatService.verifyCode(email, code);
+
+    if (isValid) {
+      res.json({ success: true, message: 'Code validé avec succès !' });
+    } else {
+      res.json({ success: false, message: 'Code incorrect.' });
+    }
+  }
 }
--- src/dash-electeur/dash-electeur.module.ts
+++ src/dash-electeur/dash-electeur.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DashElecteurController } from './dash-electeur.controller';
 import { DashElecteurService } from './dash-electeur.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Candidat } from 'src/entities/candidat.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Candidat])],
   controllers: [DashElecteurController],
   providers: [DashElecteurService]
 })
