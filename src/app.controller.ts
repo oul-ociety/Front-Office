ac0f4307fb14a4e8919184a567c32311e3836373
MoDiouf <mohameddiouf096@gmail.com>
2025-03-03 12:20:31 +0000

    okayyy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index') // Assurez-vous que le fichier "acceuil.ejs" existe dans "views"
+  @Render('accueil') // Assurez-vous que le fichier "acceuil.ejs" existe dans "views"
   home() {
     return { message: 'Bienvenue sur JollofNet !' }; // 👈 Passer des données à EJS
   }
