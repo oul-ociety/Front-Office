8570330fb77803f421b35b384e8c37661237bf5b
Mohamed <mohameddiouf096@gmail.com>
2025-03-18 15:22:44 +0100

    okayy

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -12,7 +12,7 @@ export class DashboardController {
   async home(@Req() req: Request) {
     const currentPage = req.url.replace('/dashboard', '');
     console.log('Current page:', currentPage);
-    const data = await this.dashService.getParrainage(1); 
+    const data = await this.dashService.getParrainage(3); 
     console.log("Parrainage", data);
     
 
--- src/entities/Electeur.entity.ts
+++ src/entities/Electeur.entity.ts
@@ -6,38 +6,14 @@ export class Electeur {
     @PrimaryGeneratedColumn()
     id_electeur: number;
 
-    @Column({ length: 50 })
-    nom: string;
-
-    @Column({ length: 50 })
-    prenom: string;
-
-    @Column({ length: 100, unique: true })
-    email: string;
-
     @Column({ type: 'datetime' })
     date_inscription: Date;
-
-    @Column({ type: 'tinyint' })
-    statut_validation: boolean;
-
-    @Column({ type: 'date', nullable: true })
-    ddn: Date;
-
-    @Column({ length: 255, nullable: true })
-    numBu: string;
 
     @Column()
     codeAuth: number;
 
     @Column({ length: 20, nullable: true })
     numTel: string;
-
-    @Column({ length: 255, nullable: true })
-    numEl: string;
-
-    @Column({ length: 255, nullable: true })
-    numCNI: string;
 
     @Column({ type: 'tinyint' })
     statut_parrainage: boolean;
